12cbf1ed4ccc0afb176b96df2ab3c2fda1093702
DAEKII <hhhh8915@gmail.com>
2021-08-05 17:34:30 +0900

    환경세팅 완료

---
+++ src/test/java/com/cos/dblog/DblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.dblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
